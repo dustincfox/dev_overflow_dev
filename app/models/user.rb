e25aeaccc0cc7f4714e3a0645ffc04a2b5b1f8a7
BATMAN <iamthenight@mail.com>
2014-03-12 16:26:37 -0500

    added routes for answers

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts
+  has_many :answers
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140312161833) do
+ActiveRecord::Schema.define(version: 20140312211552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "subject"
@@ -22,7 +30,6 @@ ActiveRecord::Schema.define(version: 20140312161833) do
     t.integer  "votes",           default: 0
     t.boolean  "open",            default: true
     t.integer  "favorite_answer"
-    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
