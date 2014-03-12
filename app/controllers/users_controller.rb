f51734aede3ca36d7faa18c4d85a8074183afe07
BATMAN <iamthenight@mail.com>
2014-03-12 16:12:14 -0500

    fixed user show and caught errors if invalid user id is passed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-    @posts = @user.posts
+    @user = User.where(id: params[:id]).first
+    if @user
+      @posts = @user.posts
+    else
+      redirect_to root_path
+    end
   end
 end
