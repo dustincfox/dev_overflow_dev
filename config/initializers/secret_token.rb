75058729c346e164d8a5f6ae17a135820d6c6c2c
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-03-12 10:10:57 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DbcOverflow::Application.config.secret_key_base = '60b37947a5bf514c3bf383186f394a1d21b16149f88f041ef03d8d89483d117cb524cb7a63cdd35d235f8e1ed08b6107c555d27cc27e678d71f62d553abeb5f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DbcOverflow::Application.config.session_store :cookie_store, key: '_dbc_overflow_session'
